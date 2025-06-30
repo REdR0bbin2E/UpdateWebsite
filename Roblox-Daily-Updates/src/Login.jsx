f92bc1ca70fcc25c1f6721ab9f9269a56b61eafa
REdR0bbin2E <mayo.akin2@gmail.com>
2025-06-30 13:50:15 -0500

    small changes

--- Roblox-Daily-Updates/src/Login.jsx
+++ Roblox-Daily-Updates/src/Login.jsx
@@ -8,8 +8,17 @@ import { animate, AnimatePresence, motion, scale } from 'framer-motion'
 
 function Login() {
 
-    const [signInOnTop, setSignInOnTop] = useState(false)
+    function inputValidation(props) {
+        if (name == '' || email == '' || projectKey == '') {
+        }
 
+    }
+
+
+    const [signInOnTop, setSignInOnTop] = useState(false)
+    const [name, setName] = useState('')
+    const [projectKey, setProjectKey] = useState('')
+    const [email, setEmail] = useState('')
 
     return (
         <>
@@ -46,10 +55,11 @@ function Login() {
                 <h3 style={{ fontSize: "2rem" }}>Sign in with email</h3>
 
                 <p style={{ marginTop: "0.25rem", fontSize: "1.1rem" }}>Input a key to join an existing collaborative roblox project.</p>
-                <input placeholder="Email" style={{ marginTop: "1rem", borderRadius: 25, width: "100%", padding: "10px", fontSize: "1rem", marginBottom: "1rem" }} />
-                <input placeholder="Project Key" style={{ borderRadius: 25, width: "100%", padding: "10px", fontSize: "1rem" }} />
+                <input onChange={i => setEmail(i.target.value)} placeholder="Email" style={{ marginTop: "1rem", borderRadius: 25, width: "100%", padding: "10px", fontSize: "1rem", marginBottom: "1rem" }} />
+                <input onChange={i => setName(i.target.value)} placeholder="Name" style={{ marginTop: "1rem", borderRadius: 25, width: "100%", padding: "10px", fontSize: "1rem", marginBottom: "1rem" }} />
+                <input onChange={i => setProjectKey(i.target.value)} placeholder="Project Key" style={{ marginTop: "1rem", borderRadius: 25, width: "100%", padding: "10px", fontSize: "1rem" }} />
                 <p>forgot project key</p>
-                <button style={{ borderRadius: 25, paddingLeft: "20%", paddingRight: "20%" }}>Navigate To Project</button>
+                <button onClick={inputValidation} style={{ borderRadius: 25, paddingLeft: "20%", paddingRight: "20%" }}>Navigate To Project</button>
 
 
                 <p>or sign in with</p>
@@ -57,7 +67,7 @@ function Login() {
                 <div style={{ borderRadius: 15, width: "100%" }}>
                     {/*Google Facebook and Apple go here */}
                     <button style={{ borderTopLeftRadius: 50, borderBottomLeftRadius: 50 }}>Google</button>
-                    <button style={{ borderRadius: 0 }}>Facebook</button>
+                    <button style={{ borderRadius: 0 }}>Roblox</button>
                     <button style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0, borderTopRightRadius: 50, borderBottomRightRadius: 50 }}>Apple</button>
 
                 </div>
